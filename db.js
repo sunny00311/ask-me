6558689c2d81bcab672c80c0613bcd5aaa1e7e9f
sunny sahni <sunny.sahni.2201@gmail.com>
2026-02-07 23:09:47 +0530

    Initial commit

--- db.js
+++ db.js
@@ -1,30 +1,20 @@
-// db.js
-const mysql = require("mysql2");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-// Create MySQL connection
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "suu",
-});
+let db = null;
+const url = process.env.MONGODB_URI;
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("✅ MySQL Connected");
-});
+if (!url) {
+  throw new Error("❌ MONGODB_URI is undefined. Check .env file location.");
+}
+async function DB() {
+  if (db) return db;
 
-function insertEntry(name, email, message, callback) {
-  const query = "INSERT INTO entry (name, email, message) VALUES (?, ?, ?)";
-  db.query(query, [name, email, message], callback);
+  const conn = await mongoose.connect(url);
+  console.log("✅ MongoDB Connected:", conn.connection.host);
+
+  db = mongoose.connection.db;
+  return db;
 }
 
-function getdata(callback) {
-  const query = "select * from entry";
-  db.query(query, (err, results) => {
-    if (err) return callback(err);
-    return callback(null, results);
-  });
-}
-
-module.exports = { insertEntry, getdata };
+module.exports = DB;
--- server.js
+++
@@ -1,9 +0,0 @@
-// server.js
-const http = require("http");
-const handleRequest = require("./router");
-
-const server = http.createServer(handleRequest);
-
-server.listen(5000, () => {
-  console.log("🚀 Server running at http://localhost:5000");
-});
